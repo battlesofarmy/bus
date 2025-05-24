b88eedf9a642f793b2eab65480534c813c04b806
battlesofarmy <battlesofarmy@gmail.com>
2025-05-24 20:47:53 +0600

    ip address test

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const { Request, Response, NextFunction } = require('express');
 const allowedIPs = [
   '::1',              // localhost IPv6
   '127.0.0.1',        // localhost IPv4
-  '103.153.110.45',     // CIDR block for your campus network,
+//   '103.153.110.45',     // CIDR block for your campus network,
   '103.153.110.0/24'  // campus IP range
 ];
 
